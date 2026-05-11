99970225803bb49e5d0a8420e01eaf416e87157d
iris <1312586226@qq.com>
2026-05-11 20:24:03 +0800

    update config for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // 关键：这里必须和你的仓库名一致
-  base: '/life-imprint-vue3/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    proxy: {
-      // 将 /upload 路径代理到后端服务器
-      '/upload': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      },
-      // 将 /api 路径代理到后端服务器
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
-    }
-  }
+  base: '/life-imprint-vue3/'
 })
